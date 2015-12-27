a671bc5d8a5ce527b557984d332c4649e40233e9
Aashish Welling <aashishwelling@gmail.com>
2015-12-27 18:39:30 -0500

    Removed unnecessary Meteor.isServer

--- server/publications.js
+++ server/publications.js
@@ -1,8 +1,6 @@
 /**
  * Created by Aashish on 12/27/2015.
  */
-if(Meteor.isServer) {
-    Meteor.publish("games", function (gameId) {
-        return Games.find({_id: gameId});
-    });
-}
+Meteor.publish("games", function (gameId) {
+    return Games.find({_id: gameId});
+});
