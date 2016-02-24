43c1e71d7751df5afa60bda7b22ae4b8734d2f46
Aashish Welling <aashishwelling@gmail.com>
2016-02-24 14:21:26 -0500

    Renamed clock fields

--- client/game/helpers.js
+++ client/game/helpers.js
@@ -2,19 +2,19 @@
  * Created by Aashish on 2/21/2016.
  */
 Template.game.helpers({
-    board1White: function() {
+    board1Bottom: function() {
         return numbro(Math.ceil(Template.instance().timers.get('b1W')/1000.0)).format('00:00');
     },
 
-    board1Black: function() {
+    board1Top: function() {
         return numbro(Math.ceil(Template.instance().timers.get('b1B')/1000.0)).format('00:00');
     },
 
-    board2White: function() {
+    board2Top: function() {
         return numbro(Math.ceil(Template.instance().timers.get('b2W')/1000.0)).format('00:00');
     },
 
-    board2Black: function() {
+    board2Bottom: function() {
         return numbro(Math.ceil(Template.instance().timers.get('b2B')/1000.0)).format('00:00');
     }
 });
