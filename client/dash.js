675e4ec309122572a060fa9db60e192c8aba1fdb
Aashish Welling <aashishwelling@gmail.com>
2016-03-27 12:03:20 -0400

    Set up game layout for splash page

--- client/dash.js
+++ client/dash.js
@@ -5,8 +5,9 @@ Template.dash.onRendered(function() {
         padding: 5,
         resizer: 5,
         panels: [
-            { type: 'main', size: 200, style: pstyle, content: 'main' },
-            { type: 'right', resizable: true, style: pstyle, content: 'right' }
+            { type: 'main', size: 200, style: pstyle, content: "<div id='gamePane'></div>" },
+            { type: 'right', resizable: true, style: pstyle, content: "<div id='rightPane'></div>" }
         ]
     });
+    Blaze.render(Template.game, $('#gamePane')[0]);
 });
